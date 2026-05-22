7c29b2b8219bd7784c075e579a4f39d4f922e617
dearravan02 <sanjaypandey2334@gmail.com>
2026-05-22 13:36:13 +0530

    learn variables

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accId=1234
+let accName = "shiva" 
+var accBalance = 3221
+bankName = "SBI"
+let accState;
+
+console.log(accId);
+
+// var only work within block scope and function scope so we dont use that in updated js
+//  we can not change or update  the value of "const"
+
+console.table([accId,accName,accBalance,bankName,accState])
+
