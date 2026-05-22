5e4b74d9b9b33886f1937e5bbaf6747cb018d861
dearravan02 <sanjaypandey2334@gmail.com>
2026-05-22 16:41:14 +0530

    add 01_basics files

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,14 @@
+let value = "321"
+console.log(typeof value)
+var inNumber = Number(value) 
+console.log(typeof inNumber);
+let  status = 1
+console.log(typeof status)
+var inBool = Boolean(status)
+console.log(typeof inBool)
+console.log( inBool)
+
+/*
+ same conversion happens for others like:
+ 1. string typr => String 
+*/
