cc36d471770d307a43c2b01fba9e32b75905f60b
dearravan02 <sanjaypandey2334@gmail.com>
2026-05-22 16:12:35 +0530

    data type and type conversion done

---
+++ 02_datatypes.js
@@ -0,0 +1,10 @@
+let name = "shiva"  // string type data
+let age = 134       // number data type
+let status = true   //boolean daattype
+var value = null    // **  object data type
+var empty;          // undefined data type
+
+// we can find data type by using 
+
+console.log(typeof name);
+console.log(typeof age );
