a6714c279139a1dec8fe70c69204664859df528d
dearravan02 <sanjaypandey2334@gmail.com>
2026-05-28 03:14:17 +0530

    intoduction of function done

---
+++ 04_function/01_create.js
@@ -0,0 +1,21 @@
+function intro(){
+    console.log("Hii , I am a function !!");
+}
+intro()
+
+function usingReturn(){
+    return console.log("I am come from retrun");
+    
+}
+usingReturn()
+
+function addTwo(num1, num2) {
+    return num1+num2;
+}
+console.log(addTwo(3,4));
+function nextType(name){
+        return `hiii I am ${name}`;
+    
+}
+console.log(nextType("shiva"));
+console.log(nextType("anisha"));
