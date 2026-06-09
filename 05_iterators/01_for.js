a4bc28c00bdbc53a647816c1ec6e3c396a1e4015
dearravan02 <sanjaypandey2334@gmail.com>
2026-06-10 01:54:10 +0530

    selectors done

--- 05_iterators/01_for.js
+++ 05_iterators/01_for.js
@@ -1,6 +1,22 @@
-const nums =[1,2,43,5]
+const nums =[1,2,4,3,5]
 for (let i = 0; i < nums.length; i++) {
     const element = nums[i];
-    console.log(element);
+    //console.log(element);
     
 }
+//forEach function iterate to every number in array and perform some opr to each value 
+nums.forEach(function(val){
+    //console.log(val+ " i'm forEach");
+     
+})
+//map function iterate to every number in array and perform some opr to each value like forEach but stores
+// value in new array
+
+const copyNums = nums.map((val)=>{
+//    console.log(val*3); 
+return val+2
+})
+console.log(copyNums);
+
+console.log(nums);
+
--- 05_iterators/03_arrLoop.js
+++ 05_iterators/03_arrLoop.js
@@ -1,5 +1,22 @@
 const nums = [3,4,5,6,7]
 for (const num of nums) {
-    console.log(num);
+    //console.log(num);
+    
+}
+
+const name = "Aman Kumar"
+for (const char of name) {
+    //console.log(char);
+    
+}
+
+const mp = new Map();
+mp.set("id1",123)
+mp.set("id2",654)
+for (const key of mp) {
+    //console.log(key); 
+}
+for (const key,value of mp) {
+    console.log(key,":",value);
     
 }
